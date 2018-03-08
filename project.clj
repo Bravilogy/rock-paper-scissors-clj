7fc9f7339f4275aa5f9777eb24c9d698a9dd15e1
Gega Nizharadze <gega.nizharadze@amido.com>
2018-03-08 15:20:49 +0000

    minor updates

--- project.clj
+++ project.clj
@@ -1,9 +1,7 @@
-(defproject rock-paper-scissorc "0.0.1"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject rock-paper-scissors "0.0.1"
+  :description "An awesome game"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main rps.core
   :profiles {:uberjar {:aot :all}}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.stuartsierra/component "0.3.2"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]])
